73495bf4e9f2e08f8f293b546813bf668caa599a
root <root@shurik-desktop.(none)>
2010-08-10 16:59:23 +0400

    first add

---
+++ app.js
@@ -0,0 +1,50 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    connect = require('connect');
+
+var app = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.use(connect.bodyDecoder());
+    app.use(connect.methodOverride());
+    app.use(connect.compiler({ src: __dirname + '/public', enable: ['less'] }));
+    app.use(app.router);
+    app.use(connect.staticProvider(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+    app.use(connect.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+   app.use(connect.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+    res.render('index.jade', {
+        locals: {
+            title: 'Express'
+        }
+    });
+});
+
+app.listen(3000);
+
+
+var io = require('socket.io');
+io = io.listen(app);
+//var sys = require('sys');
+io.on('connection', function(client){
+    //console.log(client);
+    // ok!!!
+    //...
+});
