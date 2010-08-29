ee624f58fed52ff8e3ff5d2191dd3c65cd1f5aa7
root <root@shurik-desktop.(none)>
2010-08-29 18:23:27 +0400

    test loginza and mongoose

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+//EXPRESS_ENV=production|development node app.js
 
 /**
  * Module dependencies.
  */
 
-var express = require('express'),
-    connect = require('connect');
+var express = require('express');
+//var connect = require('connect');
+var MemoryStore = require('connect/middleware/session/memory');
 
 var app = express.createServer();
 
@@ -12,29 +14,60 @@ var app = express.createServer();
 
 app.configure(function(){
     app.set('views', __dirname + '/views');
-    app.use(connect.bodyDecoder());
-    app.use(connect.methodOverride());
-    app.use(connect.compiler({ src: __dirname + '/public', enable: ['less'] }));
+    app.use(express.bodyDecoder());
+    app.use(express.methodOverride());
+    app.use(express.compiler({ src: __dirname + '/public', enable: ['less', 'sass'] })); // less, sass
+    app.use(express.cookieDecoder());
+    app.use(express.session({ store: new MemoryStore({ reapInterval: 60000 * 10, maxAge: 60000 * 30 }) }));
     app.use(app.router);
-    app.use(connect.staticProvider(__dirname + '/public'));
+    app.use(express.staticProvider(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-    app.use(connect.errorHandler({ dumpExceptions: true, showStack: true })); 
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-   app.use(connect.errorHandler()); 
+   app.use(express.errorHandler()); 
 });
 
 // Routes
 
 app.get('/', function(req, res){
-    res.render('index.jade', {
+    res.render('index.jade');
+});
+
+app.get('/room', function(req, res){
+    res.render('room.jade', {
         locals: {
-            title: 'Express'
+            title: 'Some room'
         }
     });
+});
+
+app.post('/login', function(req, res){
+	if (req.body.token) {
+		// http://loginza.ru/api/authinfo?token=[TOKEN_KEY_VALUE]
+		var http = require('http');
+		var loginza = http.createClient(80, 'loginza.ru');
+		var request = loginza.request('GET', '/api/authinfo?token='+req.body.token, {'host': 'localhost'});
+		request.end();
+		request.on('response', function (response) {
+		  //console.log('STATUS: ' + response.statusCode);
+		  //console.log('HEADERS: ' + JSON.stringify(response.headers));
+		  //response.setEncoding('utf8');
+		  response.on('data', function (chunk) {
+			//console.log('BODY: ' + chunk);
+//console.log(JSON.parse(chunk));
+// {"error_type":"token_validation","error_message":"Empty token value."}
+// JSON.parse(chunk)
+res.render('login.jade', {locals: {json: chunk}}); // temporary, after be redirect
+		  });
+		});
+	} else {
+		// not found *token*
+		res.render('login.jade');
+	}
 });
 
 app.listen(3000);
@@ -72,4 +105,14 @@ io.on('connection', function(client){
 setInterval(function(){
 //	//console.log(io.clients)
 }, 1000);
+*/
+
+/*
+var mongoose = require('mongoose').Mongoose;
+var db = mongoose.connect('mongodb://localhost/quiz');
+//console.log(db);
+require('./libs/user');
+mongoose.model('User', UserModel);
+var User = db.model('User');
+User.find().all(function(res){console.log(res)})
 */
---
+++ libs/user.js
@@ -0,0 +1,36 @@
+UserModel = {
+	properties: [
+		'nickname', 'email', 'first_name', 'last_name', 'age', 'created_at', 'updated_at',
+		 'identity', 'provider' # loginza
+	],
+	cast: {
+		age: Number//,
+		//'nested.path': String
+	},
+	indexes: ['nickname'],
+	setters: {
+		first_name: function(v){
+			return this.v.capitalize();
+		}
+	},
+	getters: {
+		full_name: function(){ 
+			return this.first_name + ' ' + this.last_name
+		}
+	},
+	methods: {
+		save: function(fn){
+			var cur_date = new Date();
+			if (this.isNew) {
+				this.created_at = cur_date;
+			}
+			this.updated_at = cur_date;
+			this.__super__(fn);
+		}
+	},
+	static: {
+		findNative: function(){
+			return this.find({provider: ''});
+		}
+	}
+}
